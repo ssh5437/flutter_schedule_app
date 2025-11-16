4cd1bc14887cbe9c4d1fc637cc213b7998abd17b
ssh5437 <ssh5437@gmail.com>
2025-11-16 17:15:36 +0900

    확정 스케줄 배경 색을 흰색으로 변경

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         applicationId = "com.vividlife.bizplan"
         minSdk = 24  // Android 7.0 이상
         targetSdk = 36  // Android 16
-        versionCode = 7
-        versionName = "1.0.6"
+        versionCode = 8
+        versionName = "1.0.7"
     }
 
     signingConfigs {
