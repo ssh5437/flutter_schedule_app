6c5bb20474f3a57ff4b5c1f4573b5937b4af90a2
ssh5437 <ssh5437@gmail.com>
2025-11-23 19:16:56 +0900

    스케줄 수정시 에러 발생 수정

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         applicationId = "com.vividlife.bizplan"
         minSdk = 24  // Android 7.0 이상
         targetSdk = 36  // Android 16
-        versionCode = 10
-        versionName = "1.0.9"
+        versionCode = 11
+        versionName = "1.0.11"
     }
 
     signingConfigs {
@@ -52,6 +52,11 @@ android {
     buildTypes {
         release {
             signingConfig = signingConfigs.getByName("release")
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
 }
